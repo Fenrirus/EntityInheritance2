876945a0ab3dadded8b21f8486a1d9c3ffc542a7
Marian Marian <fenrir55@gmail.com>
2018-12-09 17:43:07 +0100

    wersja 1

---
+++ EntityInheritance2/WebForm1.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="EntityInheritance2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:RadioButtonList ID="RadioButtonList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="RadioButtonList1_SelectedIndexChanged">
+                <asp:ListItem Value="Permament"></asp:ListItem>
+                <asp:ListItem>Contract</asp:ListItem>
+                <asp:ListItem>All</asp:ListItem>
+            </asp:RadioButtonList>
+             <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ EntityInheritance2/WebForm1.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace EntityInheritance2
+{
+    public partial class WebForm1 : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private DataTable Dane(List<Employees7> employees)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("ID");
+            dt.Columns.Add("FirstName");
+            dt.Columns.Add("LastName");
+            dt.Columns.Add("Gender");
+            dt.Columns.Add("AnuualSalary");
+            dt.Columns.Add("HourlyPay");
+            dt.Columns.Add("HoursWorked");
+            dt.Columns.Add("Type");
+            foreach(Employees7 employee in employees)
+            {
+                DataRow dr = dt.NewRow();
+                dr["ID"] = employee.EmployeeID;
+                dr["FirstName"] = employee.FirstName;
+                dr["LastName"] = employee.LastName;
+                dr["Gender"] = employee.Gender;
+
+                if (employee is PermanentEmployees7)
+                {
+                    dr["AnuualSalary"] = ((PermanentEmployees7)employee).AnnualSalary;
+                    dr["Type"] = "Permanent";
+                }
+                else
+                {
+                    dr["HourlyPay"] = ((ContractEmployees7)employee).HourlyPay;
+                    dr["HoursWorked"] = ((ContractEmployees7)employee).HoursWorked;
+                    dr["Type"] = "Contract";
+                }
+                dt.Rows.Add(dr);
+            }
+            return dt;
+        }
+
+        protected void RadioButtonList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            EmployeeDbcontext db = new EmployeeDbcontext();
+            switch (RadioButtonList1.SelectedValue)
+            {
+                case "Permament":
+                    {
+                        GridView1.DataSource = db.Employees7.OfType<PermanentEmployees7>().ToList();
+                        GridView1.DataBind();
+                        break;
+                    }
+                case "Contract":
+                    {
+                        GridView1.DataSource = db.Employees7.OfType<ContractEmployees7>().ToList();
+                        GridView1.DataBind();
+                        break;
+                    }
+                default:
+                    {
+                        GridView1.DataSource = Dane(db.Employees7.ToList());
+                        GridView1.DataBind();
+                        break;
+                    }
+            }
+        }
+    }
+}
